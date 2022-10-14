0af03ad679f9e52bce24fac5e4d5da705fe7044d
joan <joanflores905@gmail.com>
2022-10-14 13:23:15 +0000

    generate db starwars

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User, Personaje, Planeta, Usuario
+from models import db, User, Personaje, Planeta, Usuario, Favorito
 #from models import Person
 app = Flask(__name__)
 app.url_map.strict_slashes = False
